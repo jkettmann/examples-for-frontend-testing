fdac8b0169a06077b7738cdd5e4ff31fc20c9821
Johannes Kettmann <jkettmann@gmx.net>
2020-02-14 17:01:40 +0100

    Adjust usage of quotes

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 import axios from 'axios';
-import { render, fireEvent } from "@testing-library/react";
-import App from "./App";
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
 
 jest.mock('axios');
 
-describe("App", () => {
-  test("renders search results", async () => {
+describe('App', () => {
+  test('renders search results', async () => {
     axios.get.mockResolvedValue([
-      { id: 1, text: "First search result" },
-      { id: 2, text: "Second search result" },
-      { id: 3, text: "Third search result" }
+      { id: 1, text: 'First search result' },
+      { id: 2, text: 'Second search result' },
+      { id: 3, text: 'Third search result' }
     ]);
 
     const { findByPlaceholderText, findByText, getByText } = render(<App />);
 
-    const searchInput = await findByPlaceholderText("Search value");
-    fireEvent.change(searchInput, { target: { value: "search-string" } });
+    const searchInput = await findByPlaceholderText('Search value');
+    fireEvent.change(searchInput, { target: { value: 'search-string' } });
 
-    const searchButton = getByText("Search");
+    const searchButton = getByText('Search');
     fireEvent.click(searchButton);
 
-    expect(axios.get).toHaveBeenCalledWith("https://some-api.com/search-string");
+    expect(axios.get).toHaveBeenCalledWith('https://some-api.com/search-string');
 
-    await findByText("First search result");
-    await findByText("Second search result");
-    await findByText("Third search result");
+    await findByText('First search result');
+    await findByText('Second search result');
+    await findByText('Third search result');
   });
 });
